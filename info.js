fb8a7bdaafd28e9c8065a1632121943afd072274
yarwicsr <yarwicsr@miamioh.edu>
2022-05-11 19:44:22 -0400

    Update info.js

--- info.js
+++ info.js
@@ -27,7 +27,7 @@ function validateType() {
 }
 
 function getLocation() {
-$("#direction").empty();
+    $("#direction").empty();
 
     var add1 = $("#add1").val();
     var add2 = $("#add2").val();
@@ -40,6 +40,7 @@ $("#direction").empty();
         lat1 = data.results[0].position.lat;
         lon1 = data.results[0].position.lon;
     }).fail(function(error) {});
+
     a = $.ajax({
         url: 'https://api.tomtom.com/search/2/search/' + add2 + '.json?radius=1000&minFuzzyLevel=1&maxFuzzyLevel=2&view=Unified&relatedPois=off&key=Ti1bXZtwMA5Lx8WMxmTIIlLGLZGEKXu4',
         method: "GET"
@@ -65,27 +66,23 @@ function getDirections() {
     }).done(function(data) {
 
         $("#dist").html("Total Distance: " + data.routes[0].summary.lengthInMeters + " meters");
-        $("#time").html("Total Time: " + data.routes[0].summary.travelTimeInSeconds/60 + " min");
+        $("#time").html("Total Time: " + data.routes[0].summary.travelTimeInSeconds / 60 + " min");
         $("#traffic").html("Traffic Delay: " + data.routes[0].summary.trafficDelayInSeconds + " sec");
 
-								var oldTime = 0;
-								var oldLen = 0;
-        var i = 0;
+        var oldTime = 0;
+        var oldLen = 0;
 
         for (var i = 0; i < 99; i++) {
-var url = 'https://api.tomtom.com/map/1/staticimage?layer=basic&style=main&format=jpg&zoom=12&center=' + data.routes[0].legs[0].points[i].longitude + ',' + data.routes[0].legs[0].points[i].latitude + '&width=512&height=512&view=Unified&key=Ti1bXZtwMA5Lx8WMxmTIIlLGLZGEKXu4';
+            var url = 'https://api.tomtom.com/map/1/staticimage?layer=basic&style=main&format=jpg&zoom=12&center=' + data.routes[0].legs[0].points[i].longitude + ',' + data.routes[0].legs[0].points[i].latitude + '&width=512&height=512&view=Unified&key=Ti1bXZtwMA5Lx8WMxmTIIlLGLZGEKXu4';
 
-$("#direction").append("<br><p id='leg'>" + (i+1) + ". " + data.routes[0].guidance.instructions[i].message + "</p>");
-$("#direction").append("<img src=" + url + " alt='mapImage'>");
+            $("#direction").append("<br><p id='leg'>" + (i + 1) + ". " + data.routes[0].guidance.instructions[i].message + "</p>");
+            $("#direction").append("<img src=" + url + " alt='mapImage'>");
 
-$("#direction").append("<p id='legLength'> Length: " + (data.routes[0].guidance.instructions[i].routeOffsetInMeters -oldLen) + " meters</p>");  
-oldLen = data.routes[0].guidance.instructions[i].routeOffsetInMeters;
+            $("#direction").append("<p id='legLength'> Length: " + (data.routes[0].guidance.instructions[i].routeOffsetInMeters - oldLen) + " meters</p>");
+            oldLen = data.routes[0].guidance.instructions[i].routeOffsetInMeters;
 
-$("#direction").append("<p id='legTime'> Time: " + (data.routes[0].guidance.instructions[i].travelTimeInSeconds - oldTime)/60 + " min</p>");
-oldTime = data.routes[0].guidance.instructions[i].travelTimeInSeconds
-
-
+            $("#direction").append("<p id='legTime'> Time: " + (data.routes[0].guidance.instructions[i].travelTimeInSeconds - oldTime) / 60 + " min</p>");
+            oldTime = data.routes[0].guidance.instructions[i].travelTimeInSeconds
         }
     }).fail(function(error) {});
-
 }
